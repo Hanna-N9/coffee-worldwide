4cd3ed22007bbf20cf456b0167c70b8bce190c3f
Hanna Negash <hnegash.n@gmail.com>
2023-12-05 11:10:30 -0800

    Update home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,7 @@
 import React from "react";
-import Header from "./Header";
-import CoffeeList from "./CoffeeList";
 
-function HomePage( { coffees } ){
-    return (
-        <div>
-            <Header />
-            <CoffeeList 
-                coffees={coffees}
-            />
-        </div>
-    )
+function HomePage() {
+  return <h1>Home page</h1>;
 }
 
 export default HomePage;
