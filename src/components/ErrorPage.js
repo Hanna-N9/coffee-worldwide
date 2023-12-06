c1ef940d8ad99d40acc5a301b72d591143b27de9
Hanna Negash <hnegash.n@gmail.com>
2023-12-06 09:24:57 -0800

    Add errors info

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -1,6 +1,18 @@
-import React from "react";
+import { useRouteError } from "react-router-dom";
 
-function ErrorPage(){
+function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <div id="error-page">
+      <h1>Oops!</h1>
+      <h1>Sorry, something went wrong!</h1>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </div>
+  );
 }
 
 export default ErrorPage;
