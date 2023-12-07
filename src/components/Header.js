be09dac304fbf5b63cf7c881794e3d14640bf805
Meaghan <meaghan.sharrard89@gmail.com>
2023-12-07 11:51:53 -0800

    updated css on homepage, added a gif, and organized the css component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,37 @@
-import React from "react";
-import logo from "../images/logo.png";
+import React, { useEffect, useRef } from "react";
+import { useLocation } from "react-router-dom";
+import Gif from "../images/Bean.mp4";
 
 function Header() {
+  const videoRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    if(videoRef.current.currentTime === 0){
+      videoRef.current.play()
+    } else {
+      videoRef.current.pause()
+      videoRef.current.currentTime = 0
+      videoRef.current.play()
+    }
+
+    return () => {
+      if (videoRef.current) {
+          videoRef.current.pause();
+      }
+    };
+  }, [location]);
   
   return (
-    <div className="logobuffer">
-      <img className="logo" src={logo} alt="logo" />
+    <div className="header-container">
+      <video
+        className="logo"
+        height="500"
+        muted
+        ref={videoRef}
+      >
+        <source src={Gif} type="video/mp4" />
+      </video>
     </div>
   );
   
