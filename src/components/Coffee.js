73d8fe2bcb39c17ce36e0e7e0fc09cf1fcd11d9a
Hanna Negash <hnegash.n@gmail.com>
2023-12-07 14:18:32 -0800

    Add an id name

--- src/components/Coffee.js
+++ src/components/Coffee.js
@@ -3,22 +3,16 @@ import { Link } from "react-router-dom";
 import "../App.css";
 
 function Coffee({ coffee }) {
-
   return (
     <div>
-      <h3>{coffee.name}</h3>
+      <h3 id="cardTitle">{coffee.name}</h3>
       <div id="card">
         <Link to={`/coffee/${coffee.id}`}>
-          <img
-            src={coffee.image}
-            alt={coffee.name}
-            className="coffee-image"
-          />
+          <img src={coffee.image} alt={coffee.name} className="coffee-image" />
         </Link>
       </div>
     </div>
   );
-  
 }
 
 export default Coffee;
